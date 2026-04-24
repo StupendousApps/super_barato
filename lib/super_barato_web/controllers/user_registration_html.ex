a06f4c018816f846a708ab062dbc402258755b5d
Francisco Ceruti <hello@fceruti.com>
2026-04-24 00:23:18 -0400

    mix phx.gen.auth Accounts User users

---
+++ lib/super_barato_web/controllers/user_registration_html.ex
@@ -0,0 +1,5 @@
+defmodule SuperBaratoWeb.UserRegistrationHTML do
+  use SuperBaratoWeb, :html
+
+  embed_templates "user_registration_html/*"
+end
---
+++ lib/super_barato_web/controllers/user_settings_html.ex
@@ -0,0 +1,5 @@
+defmodule SuperBaratoWeb.UserSettingsHTML do
+  use SuperBaratoWeb, :html
+
+  embed_templates "user_settings_html/*"
+end
